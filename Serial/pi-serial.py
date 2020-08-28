8e916edb0a69b875cafa075b99db50be9cad0f0b
Sean Landers <sean.landers@gmail.com>
2020-08-28 17:36:05 -0500

    testing

--- Serial/pi-serial.py
+++ Serial/pi-serial.py
@@ -8,61 +8,61 @@ arduino = findArduino()
 ser = serial.Serial(str(arduino), 9600)
 
 def prepWeather(credential):
-	weather = swt.weatherReport(credential)
-	weatherMsg = ""
-	for key in weather.keys():
-		if counter == 0:
-			weatherMsg = str(weather[key])
-			counter += 1
-		else:
-			weatherMsg = weatherMsg + ", " + str(weather[key])
-	weatherMsg = weatherMsg + "\0"
-	return weatherMsg
+    weather = swt.weatherReport(credential)
+    weatherMsg = ""
+    for key in weather.keys():
+        if counter == 0:
+            weatherMsg = str(weather[key])
+            counter += 1
+        else:
+            weatherMsg = weatherMsg + ", " + str(weather[key])
+    weatherMsg = weatherMsg + "\0"
+    return weatherMsg
 
 def decodeSerial(line, credential):
-	print("Received: ", line)
-	if b"1" in line:
-		weather = prepWeather(credential)
-		response = weather
-	if b"2" in line:
-		response = "Waiting . . . "
-	else:
-		print("Received: ", line)
-		response = line
-	return response
+    print("Received: ", line)
+    if b"1" in line:
+        weather = prepWeather(credential)
+        response = weather
+    if b"2" in line:
+        response = "Waiting . . . "
+    else:
+        print("Received: ", line)
+        response = line
+    return response
 
 def encodeSerial(response):
-	message = response.encode()
-	return message
+    message = response.encode()
+    return message
 
 def sendSerial(message, serialobj):
-	try:
-		serialobj.write(message)
-		return True, (message.decode() + "message sent")
-	except Exception as instance:
-		return (False, instance)
+    try:
+        serialobj.write(message)
+        return True, (message.decode() + "message sent")
+    except Exception as instance:
+        return (False, instance)
 
 if __name__ == '__main__':
-#	weather = swt.weatherReport(credentials)
-#	print(weather.keys())
-	while 1:
-	    if(ser.in_waiting > 0):
-	        line = ser.readline()
-	        print(line)
-	        response = decodeSerial(line, credentials)
-	        message = encodeSerial(response)
-	        messageSent = sendSerial(message, ser)
-	        if messageSent[0] == True:
-	        	print("Sent ",message)
-	        else:
-	        	print(messageSent[1])
-        	time.sleep(5000)
-	    else:
-        	response = decodeSerial(("weather".encode), credentials)
-        	message = encodeSerial(response)
-        	messagesent = sendSerial(message, ser)
-        	if messageSent[0] == True:
-        		print("Sent ", message)
-        	else:
-        		print(messageSent[1])
-    		time.sleep(5)
+#    weather = swt.weatherReport(credentials)
+#    print(weather.keys())
+    while 1:
+        if(ser.in_waiting > 0):
+            line = ser.readline()
+            print(line)
+            response = decodeSerial(line, credentials)
+            message = encodeSerial(response)
+            messageSent = sendSerial(message, ser)
+            if messageSent[0] == True:
+                print("Sent ",message)
+            else:
+                print(messageSent[1])
+            time.sleep(5000)
+        else:
+            response = decodeSerial(("weather".encode), credentials)
+            message = encodeSerial(response)
+            messagesent = sendSerial(message, ser)
+            if messageSent[0] == True:
+                print("Sent ", message)
+            else:
+                print(messageSent[1])
+            time.sleep(5)
