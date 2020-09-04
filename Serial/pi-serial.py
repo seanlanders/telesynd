58aa36f730b04b4f8ff945161ecb8599afb58c74
Sean Landers <sean.landers@gmail.com>
2020-09-04 15:59:47 -0500

    added comments to pi-serial

--- Serial/pi-serial.py
+++ Serial/pi-serial.py
@@ -1,12 +1,24 @@
 import serial
+#TODO rename from clearly temp 'test' name
 import serialweathertest as swt
 from findArduino import findArduino
 from credentials import credentials
 import time
 
+# might be good to store baud externally in config file
+# common reference for Pi + Arduino
+baud = 57600
 arduino = findArduino()
-ser = serial.Serial(str(arduino), 57600)
+ser = serial.Serial(str(arduino), baud)
 
+# draws on swt weatherReport to retrieve weather conditions
+# possible fields, at present:
+# 'all' returns conditions, shortconditions, temperature, feelslike, humidity 
+# 'conditions' - returns 'conditions' and 'shortconditions', which describe weather in plaintext
+# 'temperature' - returns temp and feelslike in F
+## TODO add flag for F or C - controlled by external toggle?
+# 'humidity' - returns humidity %
+# all values returned as dictionary, each entry as string
 def prepWeather(fields, credential):
     weather = swt.weatherReport((fields), credential)
     weatherMsg = ""
@@ -21,6 +33,10 @@ def prepWeather(fields, credential):
     weatherMsg = "<" + weatherMsg + ">"
     return weatherMsg
 
+# responds to code from Arduino 
+# numeric code determines what should be returned
+# TODO build out
+# this + next three should probably get externalized and imported
 def decodeSerial(line, credential):
     print("Received: ", line)
     if b"1" in line:
@@ -37,10 +53,14 @@ def decodeSerial(line, credential):
     print(response)
     return response
 
+# encodes response for sending to Arduino
+# may need to be more complex & include formatting
 def encodeSerial(response):
     message = response.encode('utf-8')
     return message
 
+# sends bytes to Arduino; reports the results
+# returns a boolean value, and message that was sent (+ error if not sent)
 def sendSerial(message, serialobj):
     try:
         serialobj.write(message)
@@ -49,10 +69,12 @@ def sendSerial(message, serialobj):
         return (False, instance)
 
 if __name__ == '__main__':
-    weather = swt.weatherReport(("temperature"), credentials)
-    print(weather.keys())
+    # Debug code to make sure weather report works
+    #weather = swt.weatherReport(("temperature"), credentials)
+    #print(weather.keys())
     #print(weather[0])
-    print(weather["temperature"])
+    #print(weather["temperature"])
+    #
     while 1:
         if(ser.in_waiting > 0):
             line = ser.readline()
