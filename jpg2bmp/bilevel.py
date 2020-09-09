b0e06db2a3efbf5d0baf9f16b3d06d1647b47985
Sean Landers <sean.landers@gmail.com>
2020-09-09 17:15:55 -0500

    .

--- jpg2bmp/bilevel.py
+++ jpg2bmp/bilevel.py
@@ -8,7 +8,7 @@ for file in bmpFiles:
 	prefix = prefix[0]
 	target = prefix+"-bilevel.bmp"
 	img = Image.open(file)
-	thresh = 200
+	thresh = 100
 	fn = lambda x : 255 if x > thresh else 0
 	r = img.convert('L').point(fn, mode='1')
 	r.thumbnail((128,96))
@@ -17,4 +17,4 @@ for file in bmpFiles:
 	with open((prefix+"-bilevel.cpp"), "wb") as binary_file:
 		binary_file.write(inoBits)
 		binary_file.close()
-	img.save(prefix+"-bilevel.bmp")
+	r.save(prefix+"-bilevel.bmp")
